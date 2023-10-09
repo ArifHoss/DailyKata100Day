f83ccbbbe90c231858b6845ac1cae706482687d2
Arif Hossain <hossaininfo@ymail.com>
2023-10-09 14:06:42 +0200

    created a function that add number to a list

---
+++ src/week_03/LineNumbering.java
@@ -0,0 +1,29 @@
+package week_03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LineNumbering {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("a", "b", "c");
+        System.out.println(numbering(list));
+        System.out.println(numbering1(list));
+    }
+    public static List<String> numbering(List<String> lines) {
+        List<String> addNumber = new ArrayList<>();
+        for (int i = 0; i < lines.size(); i++) {
+            addNumber.add(i+1 + ": " + lines.get(i));
+        }
+        return addNumber;
+    }
+
+    public static List<String> numbering1(List<String> lines) {
+        List<String> addNumber = new ArrayList<>();
+        int i = 0;
+        for (String line : lines) {
+            addNumber.add(++i + ": " + line);
+        }
+        return addNumber;
+    }
+}
